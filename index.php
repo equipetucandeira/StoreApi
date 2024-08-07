4862eef7c6ecfaf3b95f9d23ebd05a97598077ed
Pedro Clemonini <pclemonini-00@hotmail.com>
2024-08-07 11:50:17 -0300

    header config

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 Router::execute();
 
 
