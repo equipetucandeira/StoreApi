1baaa539c067a706f74dec6ab896a4f356975c76
JoaoWSL <joaoruffino2005@gmail.com>
2024-08-10 20:51:57 -0300

    fix headers

--- index.php
+++ index.php
@@ -10,9 +10,11 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Authorization");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Authorization, USER-ID");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Expose-Headers: HTTP_USER_ID");
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     // Responder com status 200 OK e sair
